b4c26fc6baa84c4f7b6fd1caac1c1a0506355a59
ph4ge <67114670+ph4ge@users.noreply.github.com>
2020-12-06 21:52:23 +0100

    Add files via upload
    added third batch of files

---
+++ python2_Lesson02/src/messagetest.py
@@ -0,0 +1,10 @@
+"""
+Demonstrate a message formulated by the unittest system.
+"""
+
+import unittest
+
+class DemoCase(unittest.TestCase):
+    def testMessage1(self):
+        self.assertEqual([1,2,3,4], [1, 2, [3, 4]])
+
